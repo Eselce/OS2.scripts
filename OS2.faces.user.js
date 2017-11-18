e36f8f8968a7e4a28269e7d8e7af09fe652ce742
Eselce <S.Loges@gmx.de>
2017-11-18 19:52:17 +0100

    Version 0.1 (2017-11-17 00:00 0.1-SLC)

--- OS2.faces.user.js
+++ OS2.faces.user.js
@@ -1,50 +1,51 @@
-// ==UserScript==
+﻿// ==UserScript==
 // @name         OS2.faces
 // @namespace    http://os.ongapo.com/
-// @version      0.1
+// @version      0.1-SLC-WE
 // @copyright    2016+
 // @author       Roman Bauer
 // @description  Farbeauswahl über HTML5 Color Picker
-// @include      /^https?://(www\.|)(os\.ongapo\.com|online-soccer\.eu|os-zeitungen\.com)/osbetafaces\.php$/
+// @include      /^https?://(www\.)?(os\.ongapo\.com|online-soccer\.eu|os-zeitungen\.com)/osbetafaces\.php$/
+// @grant        none
 // ==/UserScript==
 
 /*jshint loopfunc: true */
 
 (function () {
 
-    "use strict";
+	"use strict";
 
-    var colorPickerAvailable = (function () {
-        var e = document.createElement("input");
-        e.setAttribute("type", "color");
-        return e.type !== "text";
-    })();
+	var colorPickerAvailable = (function () {
+		var e = document.createElement("input");
+		e.setAttribute("type", "color");
+		return e.type !== "text";
+	})();
 
-    if (colorPickerAvailable) {
+	if (colorPickerAvailable) {
 
-        var inputs = document.getElementsByTagName("input"), i;
+		var inputs = document.getElementsByTagName("input"), i;
 
-        for (i = 0; i < inputs.length; i++) {
+		for (i = 0; i < inputs.length; i++) {
 
-            var textInput = inputs[i];
+			var textInput = inputs[i];
 
-            if (textInput.name && textInput.name.search(/.*farbe.*/) !== -1 && textInput.type === "text") {
+			if (textInput.name && textInput.name.search(/.*farbe.*/) !== -1 && textInput.type === "text") {
 
-                var colorPicker = document.createElement("input");
+				var colorPicker = document.createElement("input");
 
-                colorPicker.type = "color";
-                colorPicker.value = "#" + inputs[i].value;
+				colorPicker.type = "color";
+				colorPicker.value = "#" + inputs[i].value;
 
-                colorPicker.oninput = function () {
-                    this.nextSibling.value = this.value.substr(1);
-                };
+				colorPicker.oninput = function () {
+					this.nextSibling.value = this.value.substr(1);
+				};
 
-                textInput.parentNode.insertBefore(colorPicker, textInput);
-                textInput.style.display = "none";
+				textInput.parentNode.insertBefore(colorPicker, textInput);
+				textInput.style.display = "none";
 
-                i++; // cause of new input
-            }
-        }
-    }
+				i++; // cause of new input
+			}
+		}
+	}
 
 })();
