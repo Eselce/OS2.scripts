c5d937b71ccf486f05456a5fd4ba59217a5084ae
Eselce <S.Loges@gmx.de>
2017-11-19 16:49:09 +0100

    Version 0.1 (2017-11-19 00:00 0.1)

--- OS2.vereinshistorieMitKruecke.user.js
+++ OS2.vereinshistorieMitKruecke.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name OS2.vereinshistorie
+// @name OS2.vereinshistorieMitKruecke
 // @version 0.1
-// @description OS 2.0 - Zeigt relativen und absolutem MW-Zuwachs an
+// @description OS 2.0 - Ergaenzt fehlende Zeilen und zeigt absoluten und relativen MW-Zuwachs an
 // @include http://os.ongapo.com/showteam.php?s=7
 // @include http://os.ongapo.com/st.php?s=7*
 // @include http://www.os.ongapo.com/showteam.php?s=7
@@ -13,21 +13,130 @@
 // @grant none
 // ==/UserScript==
 
+var fehlendeZeilen = new Array();
+
 // Konfiguration ************************************************************************
+// Statt "9999" bitte die eigene Vereins-ID eintragen:
+var eigeneID = 9999;
+
+// Die beiden Befehle "fehlendeZeilen.push(new Array( ... ));" unten fuegen vermisste Zeilen von Saison 6/ZAT 18 fuer Vereins-ID 9999 und 9998 hinzu.
+// Hinweise:
+// Es muessen die folgenden Werte in genau dieser Reihenfolge getrennt durch Kommata angegeben werden (Punkt als Dezimaltrennzeichen, Managername in Anfuehrungszeichen)
+// Vereins-ID, Saison, ZAT, Spieler, Skill, Opti, Alter, Schnitt MW, Summe MW, Schnitt Gehalt, Summe Gehalt, Manager
+
+fehlendeZeilen.push(new Array(9999, 6, 18, 33, 45.67, 78.90, 23.45, 12345678, 345678901, 98765, 3456789, "Fußballgott"));
+fehlendeZeilen.push(new Array(9998, 6, 18, 32, 44.97, 79.70, 23.51, 12395678, 345698901, 98965, 3459789, "Mr. Bananenflanke"));
+
+// Konfiguration des normalen Vereinshistorie-Skripts ***********************************
 var titelMwAbsolut = "\u0394MW abs."; // Titel der Spalte fuer die absolute MW-Differenz
 var titelMwRelativ = "\u0394MW rel."; // Titel der Spalte fuer die relative MW-Differenz
 var popupFensterFaktorX = 0.2; // Faktor, um den die Breite des Popupfensters vergroessert wird
 // Konfiguration Ende *******************************************************************
 
-var colorPos = "#00FF00";
-var colorNeg = "#FF0000";
-var borderString = "solid white 1px";
 var offsetTop = 2;
 var offsetBottom = 1;
 var table = document.getElementsByTagName("table")[1];
 var offsets = [offsetTop, offsetBottom, 0, 0]; // 2 Zeilen oben, 1 Zeile unten ausschliessen
-var colIdxMwSum = 7;
 
+// Kruecken-Logik ***********************************************************************
+
+var showteam = false;
+var url = this.location.href;
+var regexp = /showteam/;
+if (regexp.test(url)) { showteam = true; }
+regexp = /c=(\d+)/;
+var teamID = -1;
+if (regexp.test(url)) { teamID = parseInt(regexp.exec(url)[1]); }
+
+// Die relevanten Zeilen aus "fehlendeZeilen" ermitteln
+var relevanteZeilen = new Array();
+if (showteam) {
+    // Relevant ist alles mit der eigenen ID ("eigeneID")
+    for (var i = 0; i < fehlendeZeilen.length; i++) {
+        if (fehlendeZeilen[i][0] == eigeneID) {
+            relevanteZeilen.push(fehlendeZeilen[i]);
+        }
+    }
+} else {
+    // Relevant ist alles mit der gleichen ID wie "teamID"
+    for (var i = 0; i < fehlendeZeilen.length; i++) {
+        if (fehlendeZeilen[i][0] == teamID) {
+            relevanteZeilen.push(fehlendeZeilen[i]);
+        }
+    }
+}
+
+var colIdxSeason = 0;
+var colIdxZat = 1;
+var idxRow = offsetTop;
+var idxRowMax = table.rows.length - 1;
+var season = 0;
+var zat = 0;
+var seasonNext = 0;
+var zatNext = 0;
+
+while (idxRow < idxRowMax) {
+    // Muss eine relevante Zeile eingefuegt werden?
+    season = stringToNumber(table.rows[idxRow].cells[colIdxSeason].textContent);
+    zat = stringToNumber(table.rows[idxRow].cells[colIdxZat].textContent);
+    seasonNext = stringToNumber(table.rows[idxRow + 1].cells[colIdxSeason].textContent);
+    zatNext = stringToNumber(table.rows[idxRow + 1].cells[colIdxZat].textContent);
+    for (var i = 0; i < relevanteZeilen.length; i++) {
+        if (isEqualTime(relevanteZeilen[i][1], relevanteZeilen[i][2], season, zat)) {
+            // Relevante Zeile gibt es schon -> entfernen
+            relevanteZeilen[i][1] = -1; // Saison auf -1 setzen zum Entfernen
+        } else if (isLaterTime(relevanteZeilen[i][1], relevanteZeilen[i][2], seasonNext, zatNext)) {
+            // Relevante Zeile ist "spaeter" als die naechste -> einfuegen und dann aus "relevanteZeilen" entfernen
+            insertRow(table, relevanteZeilen[i], idxRow + 1);
+            relevanteZeilen[i][1] = -1; // Saison auf -1 setzen zum Entfernen
+            idxRowMax = table.rows.length - 1; // "idxRowMax" aktualisieren, da sich Anzahl Zeilen geaendert hat
+            break; // Aus for-Schleife aussteigen, damit max. eine relevante Zeile eingefuegt wird
+        }
+    }
+    idxRow += 1;
+}
+
+// Fuegt eine Zeile in eine Tabelle ein
+// table: Die Tabelle, in die die Zeile eingefueget werden soll
+// row: Die Zeile, die eingefuegt werden soll als Array der Zellinhalte
+// idxRow: Der Zeilenindex, an dem die neue Zeile eingefuegt werden soll
+function insertRow(table, row, idxRow) {
+    var styleString = "padding-left:10px;";
+    var textContent = "";
+    table.insertRow(idxRow);
+    for (var i = 1; i < row.length; i++) { // Start mit i = 1, um die Vereins-ID auszulassen
+        // Format des Zellinhalts bestimmen. Standard: Text
+        textContent = row[i];
+        switch (true) {
+            case ((i == 4) || (i == 5) || (i == 6)): // Dezimalzahlen
+                textContent = decimalNumberToString(row[i], 2, false);
+                break;
+            case ((i == 1) || (i == 2) || (i== 3) || (i == 7) || (i == 8) || (i == 9) || (i == 10)): // Ganze Zahlen
+                textContent = wholeNumberToString(row[i]);
+                break;
+        }
+        appendCell(table.rows[idxRow], textContent, "", "center").setAttribute("style", styleString);
+    }
+}
+
+// Gibt true zurueck, wenn die OS-Zeitpunkte gleich sind
+// s1, z1, s2, z2: Saison bzw. Zat von Argument 1 bzw. 2
+function isEqualTime(s1, z1, s2, z2) {
+    return (s1 == s2) && (z1 == z2);
+}
+
+// Gibt true zurueck, wenn die OS-Zeitpunkt 1 groesser als OS-Zeitpunkt 2 ist
+// s1, z1, s2, z2: Saison bzw. Zat von Argument 1 bzw. 2
+function isLaterTime(s1, z1, s2, z2) {
+    return (((s1 == s2) && (z1 > z2)) || (s1 > s2));
+}
+
+// Kruecken-Logik Ende ******************************************************************
+
+var colorPos = "#00FF00";
+var colorNeg = "#FF0000";
+var borderString = "solid white 1px";
+var colIdxMwSum = 7;
 var mw = 0;
 var mwPrev = 0;
 var mwDiffAbsString = "";
