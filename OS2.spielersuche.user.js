f5a91a8c9af377af4b1b5492cb1ca85610a1eb9d
Eselce <S.Loges@gmx.de>
2017-11-19 02:10:50 +0100

    Version 0.1 (2016-11-10 12:19 0.1)

---
+++ OS2.spielersuche.user.js
@@ -0,0 +1,26 @@
+﻿// ==UserScript==
+// @name         Spielersuche
+// @namespace    http://os.ongapo.com/
+// @version      0.1
+// @description  Transferdetails einblenden
+// @author       Michael Bertram
+// @include      http*://os.ongapo.com/suchspieler.php
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    var Elements, thisElement;
+    Elements = document.getElementsByTagName('option');
+    for (var i = 0; i < Elements.length; i++) {
+        thisElement = Elements[i];
+        if (thisElement.text == 'VLZ') {
+            thisElement.value = 35;
+            thisElement.text = 'TDetails';            
+        } else {
+            // nichts
+            // alert("hello world");
+        }
+    }   
+})();
