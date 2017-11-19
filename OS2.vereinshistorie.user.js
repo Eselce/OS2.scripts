ea3e1c7f50bc4f3ae724c23d5a9cb12a42013962
Eselce <S.Loges@gmx.de>
2017-11-19 16:23:00 +0100

    Version 1.0 (2017-11-15 12:58 1.0)

--- OS2.vereinshistorie.user.js
+++ OS2.vereinshistorie.user.js
@@ -42,19 +42,19 @@ var newLength = table.rows[1].cells[0].getAttribute("colspan") + 2;
 table.rows[1].cells[0].setAttribute("colspan", newLength);
 // Werte berechnen und eintragen
 for (var i = offsetTop; i < table.rows.length - offsetBottom; i++) {
-	mw = stringToNumber(table.rows[i].cells[colIdxMwSum].textContent);
-	mwPrev = stringToNumber(table.rows[i + 1].cells[colIdxMwSum].textContent);
-	mwDiffAbsString = wholeNumberToString(mw - mwPrev);
-	mwDiffRelString = decimalNumberToString((mw - mwPrev) / mwPrev, 2, true);
-	color = (mw - mwPrev >= 0) ? colorPos : colorNeg;
-	appendCell(table.rows[i], mwDiffAbsString, color, "right");
-	appendCell(table.rows[i], mwDiffRelString, color, "right");
+    mw = stringToNumber(table.rows[i].cells[colIdxMwSum].textContent);
+    mwPrev = stringToNumber(table.rows[i + 1].cells[colIdxMwSum].textContent);
+    mwDiffAbsString = wholeNumberToString(mw - mwPrev);
+    mwDiffRelString = decimalNumberToString((mw - mwPrev) / mwPrev, 2, true);
+    color = (mw - mwPrev >= 0) ? colorPos : colorNeg;
+    appendCell(table.rows[i], mwDiffAbsString, color, "right");
+    appendCell(table.rows[i], mwDiffRelString, color, "right");
 }
 // Linien zeichnen
 drawHorizontalLines(table, offsets, 1, 0);
 // Wenn Popup-Fenster, dann verbreitern
 if (isPopupWindow(this)) {
-	this.resizeBy(this.innerWidth * popupFensterFaktorX, 0);
+    this.resizeBy(this.innerWidth * popupFensterFaktorX, 0);
 }
 
 // ****************************************************************************
@@ -69,35 +69,35 @@ if (isPopupWindow(this)) {
 // mode = 3: Abrechnungsperioden trennen ("% 6 == 0")
 // colIdx: Index der Spalte, die die zu vergleichenden Werte enthaelt
 function drawHorizontalLines(table, offsets, mode, colIdx) {
-	for (var i = offsets[0]; i < table.rows.length - offsets[1]; i++) {
-		switch (mode) {
-			case 1: // Saisons trennen
-				var value1 = parseInt(table.rows[i].cells[colIdx].textContent);
-				var value2 = parseInt(table.rows[i + 1].cells[colIdx].textContent);
-				if (value1 != value2) {
-					for (var j = offsets[2]; j < table.rows[i].cells.length - offsets[3]; j++) {
-						table.rows[i].cells[j].style.borderBottom = borderString;
-					}
-				}
-				break;
-			case 2: // Abrechnungsperioden trennen (Saison 1)
-				var value = parseInt(table.rows[i].cells[colIdx].textContent);
-				if (value % 7 == 0) {
-					for (var j = offsets[2]; j < table.rows[i].cells.length - offsets[3]; j++) {
-						table.rows[i].cells[j].style.borderBottom = borderString;
-					}
-				}
-				break;
-			case 3: // Abrechnungsperioden trennen (Saisons > 1)
-				var value = parseInt(table.rows[i].cells[colIdx].textContent);
-				if (value % 6 == 0) {
-					for (var j = offsets[2]; j < table.rows[i].cells.length - offsets[3]; j++) {
-						table.rows[i].cells[j].style.borderBottom = borderString;
-					}
-				}
-				break;
-		}
-	}
+    for (var i = offsets[0]; i < table.rows.length - offsets[1]; i++) {
+        switch (mode) {
+            case 1: // Saisons trennen
+                var value1 = parseInt(table.rows[i].cells[colIdx].textContent);
+                var value2 = parseInt(table.rows[i + 1].cells[colIdx].textContent);
+                if (value1 != value2) {
+                    for (var j = offsets[2]; j < table.rows[i].cells.length - offsets[3]; j++) {
+                        table.rows[i].cells[j].style.borderBottom = borderString;
+                    }
+                }
+                break;
+            case 2: // Abrechnungsperioden trennen (Saison 1)
+                var value = parseInt(table.rows[i].cells[colIdx].textContent);
+                if (value % 7 == 0) {
+                    for (var j = offsets[2]; j < table.rows[i].cells.length - offsets[3]; j++) {
+                        table.rows[i].cells[j].style.borderBottom = borderString;
+                    }
+                }
+                break;
+            case 3: // Abrechnungsperioden trennen (Saisons > 1)
+                var value = parseInt(table.rows[i].cells[colIdx].textContent);
+                if (value % 6 == 0) {
+                    for (var j = offsets[2]; j < table.rows[i].cells.length - offsets[3]; j++) {
+                        table.rows[i].cells[j].style.borderBottom = borderString;
+                    }
+                }
+                break;
+        }
+    }
 }
 
 // Fuegt eine Zelle am Ende einer Zeile hinzu, fuellt die Zelle und gibt sie (td-Tag) zurueck.
@@ -107,11 +107,11 @@ function drawHorizontalLines(table, offsets, mode, colIdx) {
 // alignment: Ausrichtung des Texts (left, center, right)
 // Bei Aufruf ohne Farbe wird die Standardfarbe benutzt
 function appendCell(row, content, color, alignment) {
-	var returnValue = row.insertCell(-1);
-	returnValue.textContent = content;
-	returnValue.style.color = color;
-	if (alignment != "") { returnValue.align = alignment; }
-	return returnValue;
+    var returnValue = row.insertCell(-1);
+    returnValue.textContent = content;
+    returnValue.style.color = color;
+    if (alignment != "") { returnValue.align = alignment; }
+    return returnValue;
 }
 
 // Wandelt eine Dezimalzahl in einen String um ("." als Dezimalzeichen).
@@ -120,29 +120,29 @@ function appendCell(row, content, color, alignment) {
 // decimalDigits: Anzahl der Nachkommastellen in der Ausgabe (werden erzwungen)
 // percent: Ob die Zahl als Prozentwert dargestellt werden soll
 function decimalNumberToString(number, decimalDigits, percent) {
-	var returnValue = "";
-	if (percent) { number *= 100; }
-	number = number.toFixed(decimalDigits);
-	returnValue = number.toString();
-	if (percent) { returnValue += " %"; }
-	return returnValue;
+    var returnValue = "";
+    if (percent) { number *= 100; }
+    number = number.toFixed(decimalDigits);
+    returnValue = number.toString();
+    if (percent) { returnValue += " %"; }
+    return returnValue;
 }
 
 // Wandelt eine ganze Zahl in einen String um ("." als Tausenderzeichen).
 // number: Die Zahl, die umgewandelt werden soll
 function wholeNumberToString(number) {
-	var returnValue = "";
-	var temp = "";
-	var counter = 0;
-	for (var i = number.toString().length - 1; i >= 0; i--) {
-		temp += number.toString().charAt(i);
-		counter += 1;
-		if (((counter % 3) == 0) && (i > 0) && (number.toString().charAt(i - 1) != "-")) { temp += "."; }
-	}
-	for (var i = temp.length - 1; i >= 0; i--) {
-		returnValue += temp.charAt(i);
-	}
-	return returnValue;
+    var returnValue = "";
+    var temp = "";
+    var counter = 0;
+    for (var i = number.toString().length - 1; i >= 0; i--) {
+        temp += number.toString().charAt(i);
+        counter += 1;
+        if (((counter % 3) == 0) && (i > 0) && (number.toString().charAt(i - 1) != "-")) { temp += "."; }
+    }
+    for (var i = temp.length - 1; i >= 0; i--) {
+        returnValue += temp.charAt(i);
+    }
+    return returnValue;
 }
 
 // Wandelt einen String in eine Zahl um.
@@ -151,39 +151,39 @@ function wholeNumberToString(number) {
 // Dezimalzahlen werden erkannt, wenn sie mit "." gefolgt von beliebig vielen Ziffern enden.
 // Da zuerst auf ganze Zahlen geprueft wird, koennen Dezimalzahlen nicht 3 Nachkommaziffern haben.
 function stringToNumber(string) {
-	// parseXXX interpretiert einen Punkt immer als Dezimaltrennzeichen
-	var returnValue = "";
-	var percent = false;
-	// Buchstaben und Whitespaces entfernen
-	string = string.replace(/[\sa-zA-Z]/g, "");
-	// Auf % pruefen und % entfernen
-	if (string.lastIndexOf("%") != -1) {
-		percent = true;
-		string = string.replace(/%/g, "");
-	}
-	var regexpWholeSimple = /^\d+$/;
-	var regexpWholeWithDots = /^\d+(\.\d{3}){1,}$/;
-	var regexpDecimal = /^\d*\.\d{1,}$/;
-	if (regexpWholeSimple.test(string)) {
-		// Einfache ganze Zahl
-		returnValue = parseInt(string);
-	} else if (regexpWholeWithDots.test(string)) {
-		// Ganze Zahl mit Tausenderpunkten
-		returnValue = parseInt(string.replace(/\./g, ""));
-	} else if (regexpDecimal.test(string)) {
-		// Dezimalzahl mit Punkt als Trennzeichen
-		returnValue = parseFloat(string);
-	} else {
-		// Kein gueltiger String
-		percent = false;
-		returnValue = "";
-	}
-	if (percent) { returnValue /= 100; }
-	return returnValue;
+    // parseXXX interpretiert einen Punkt immer als Dezimaltrennzeichen
+    var returnValue = "";
+    var percent = false;
+    // Buchstaben und Whitespaces entfernen
+    string = string.replace(/[\sa-zA-Z]/g, "");
+    // Auf % pruefen und % entfernen
+    if (string.lastIndexOf("%") != -1) {
+        percent = true;
+        string = string.replace(/%/g, "");
+    }
+    var regexpWholeSimple = /^\d+$/;
+    var regexpWholeWithDots = /^\d+(\.\d{3}){1,}$/;
+    var regexpDecimal = /^\d*\.\d{1,}$/;
+    if (regexpWholeSimple.test(string)) {
+        // Einfache ganze Zahl
+        returnValue = parseInt(string);
+    } else if (regexpWholeWithDots.test(string)) {
+        // Ganze Zahl mit Tausenderpunkten
+        returnValue = parseInt(string.replace(/\./g, ""));
+    } else if (regexpDecimal.test(string)) {
+        // Dezimalzahl mit Punkt als Trennzeichen
+        returnValue = parseFloat(string);
+    } else {
+        // Kein gueltiger String
+        percent = false;
+        returnValue = "";
+    }
+    if (percent) { returnValue /= 100; }
+    return returnValue;
 }
 
 // Gibt true zurueck, wenn das uebergebene Fenster keine sichtbare Menueleiste hat.
 // Im Allgemeinen ist das Fenster dann ein Popupfenster.
 function isPopupWindow(aWindow) {
-	return !(aWindow.menubar && aWindow.menubar.visible);
+    return !(aWindow.menubar && aWindow.menubar.visible);
 }
